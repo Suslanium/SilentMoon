69d522049f0ad6890c9da4e68a22af5aaa5369ad
Suslanium <zaharov22m@gmail.com>
2024-03-02 21:07:56 +0700

    Home fragment pt.1

--- app/src/main/java/com/suslanium/silentmoon/HomeFragment.kt
+++ app/src/main/java/com/suslanium/silentmoon/HomeFragment.kt
@@ -5,13 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.ViewCompat
+import com.suslanium.silentmoon.databinding.FragmentHomeBinding
+import com.suslanium.silentmoon.utils.InsetListeners
 
 class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    ): View {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.logoHome, InsetListeners.marginTopListener)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.silentLogoHome, InsetListeners.marginTopListener)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.moonLogoHome, InsetListeners.marginTopListener)
+        return binding.root
     }
 }
