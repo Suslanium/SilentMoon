e5215922228c86054dbf15c567b4d744cc22a7b8
Suslanium <zaharov22m@gmail.com>
2024-03-08 21:41:28 +0700

    Bottom bar color transition & bug fix

--- app/src/main/java/com/suslanium/silentmoon/BottomBarRootFragment.kt
+++ app/src/main/java/com/suslanium/silentmoon/BottomBarRootFragment.kt
@@ -1,5 +1,8 @@
 package com.suslanium.silentmoon
 
+import android.animation.ValueAnimator
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -19,6 +22,7 @@ import com.suslanium.silentmoon.utils.navigateToFragmentBottomBar
 class BottomBarRootFragment : Fragment() {
 
     private var _binding: FragmentBottomBarRootBinding? = null
+
     private val binding get() = _binding!!
 
     private lateinit var backCallback: OnBackPressedCallback
@@ -61,6 +65,7 @@ class BottomBarRootFragment : Fragment() {
         }
 
         childFragmentManager.addFragmentOnAttachListener { _, fragment ->
+            determineTheme(fragment, false)
             handleFragmentChange(fragment) {
                 itemChangedProgrammatically = true
             }
@@ -68,6 +73,7 @@ class BottomBarRootFragment : Fragment() {
 
         childFragmentManager.addOnBackStackChangedListener {
             val currentFragment = childFragmentManager.fragments.last()
+            determineTheme(currentFragment, false)
             handleFragmentChange(currentFragment) {
                 itemChangedProgrammatically = true
             }
@@ -78,11 +84,10 @@ class BottomBarRootFragment : Fragment() {
 
     override fun onResume() {
         super.onResume()
+        determineTheme(childFragmentManager.fragments.last(), true)
         backCallback = requireActivity().onBackPressedDispatcher.addCallback {
-            if (childFragmentManager.backStackEntryCount > 0)
-                childFragmentManager.popBackStack()
-            else
-                activity?.finish()
+            if (childFragmentManager.backStackEntryCount > 0) childFragmentManager.popBackStack()
+            else activity?.finish()
         }
     }
 
@@ -93,8 +98,7 @@ class BottomBarRootFragment : Fragment() {
     }
 
     private fun handleFragmentChange(
-        fragment: Fragment,
-        onSelectedItemChanged: () -> Unit
+        fragment: Fragment, onSelectedItemChanged: () -> Unit
     ) {
         when (fragment) {
             is HomeFragment -> {
@@ -112,11 +116,85 @@ class BottomBarRootFragment : Fragment() {
                 binding.bottomBar.selectedItemId = R.id.menu_sleep
             }
 
+            is SleepMusicFragment -> {
+                onSelectedItemChanged()
+                binding.bottomBar.selectedItemId = R.id.menu_sleep
+            }
+
             is ProfileFragment -> {
                 onSelectedItemChanged()
                 binding.bottomBar.selectedItemId = R.id.menu_profile
             }
         }
+    }
+
+    private fun determineTheme(
+        fragment: Fragment, immediateTransition: Boolean
+    ) {
+        when (fragment) {
+            is HomeFragment -> setTheme(
+                dark = false, immediateTransition = immediateTransition
+            )
+
+            is MeditateV2Fragment -> setTheme(
+                dark = false, immediateTransition = immediateTransition
+            )
+
+            is SleepFragment -> setTheme(
+                dark = true, immediateTransition = immediateTransition
+            )
+
+            is SleepMusicFragment -> setTheme(
+                dark = true, immediateTransition = immediateTransition
+            )
+
+            is ProfileFragment -> setTheme(
+                dark = false, immediateTransition = immediateTransition
+            )
+        }
+    }
+
+    private fun setTheme(dark: Boolean, immediateTransition: Boolean) {
+        val animationStartColor =
+            (binding.bottomBar.background as? ColorDrawable)?.color ?: Color.WHITE
+        if (dark) {
+            binding.bottomBar.itemTextColor =
+                requireContext().getColorStateList(R.color.bottom_bar_text_color_dark)
+            binding.bottomBar.itemIconTintList =
+                requireContext().getColorStateList(R.color.bottom_bar_icon_color_dark)
+
+            val animationEndColor = requireContext().getColor(R.color.sleep_bg)
+
+            if (!immediateTransition) animateThemeColorChange(animationStartColor, animationEndColor)
+            else changeThemeColorImmediately(animationEndColor)
+        } else {
+            binding.bottomBar.itemTextColor =
+                requireContext().getColorStateList(R.color.bottom_bar_text_color)
+            binding.bottomBar.itemIconTintList =
+                requireContext().getColorStateList(R.color.bottom_bar_icon_color)
+
+            val animationEndColor = requireContext().getColor(R.color.white)
+
+            if (!immediateTransition) animateThemeColorChange(animationStartColor, animationEndColor)
+            else changeThemeColorImmediately(animationEndColor)
+        }
+    }
+
+    private fun changeThemeColorImmediately(color: Int) {
+        binding.navigationFragmentRoot.setBackgroundColor(color)
+        binding.bottomBar.setBackgroundColor(color)
+        binding.root.setBackgroundColor(color)
+    }
+
+    private fun animateThemeColorChange(startColor: Int, endColor: Int) {
+        val colorAnimation = ValueAnimator.ofArgb(startColor, endColor)
+        colorAnimation.setDuration(300)
+        colorAnimation.addUpdateListener {
+            binding.navigationFragmentRoot.setBackgroundColor(it.animatedValue as Int)
+            binding.bottomBar.setBackgroundColor(it.animatedValue as Int)
+            binding.root.setBackgroundColor(it.animatedValue as Int)
+        }
+        colorAnimation.start()
     }
 
     override fun onDestroyView() {
